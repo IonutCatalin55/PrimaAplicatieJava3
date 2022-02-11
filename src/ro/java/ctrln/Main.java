c7b6595eb9f6992475ca3eda03e8844683fb3e19
Alexandru199205 <bacescualexandru7gmail.com>
2022-02-11 03:35:50 +0200

    ooo

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,10 @@
+package ro.java.ctrln;
+
+public class Main {
+    // JAR = Java Archive =arhiva cu toate procesele de compilare existente in proiect
+
+    public static void main(String[] args)
+    {
+        System.out.println("Invat Java ,upiii");
+    }
+}
